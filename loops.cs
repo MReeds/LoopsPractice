bc3b8d65b995086a3d02c892b4fea0c01fd40ce8
Matt Reeds <mattlreed@gmail.com>
2020-10-14 10:34:49 -0500

    basic example

---
+++ loops.cs
@@ -0,0 +1,20 @@
+// For Each
+
+List<string> foods = new List<string>()
+{
+    "Brussels Sprout",
+    "Toast",
+    "Steak",
+    "Tomato"
+};
+foreach (string food in foods)
+{
+    Console.WriteLine($"You can eat {food}.");
+}
+
+// Do While
+
+while (true)
+{
+    Console.WriteLine("Never stop looping");
+}
